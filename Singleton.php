32650f92e76c3369d2709e252304ffe8106bc08c
Carl Parrish <cparrish@carlparrish.com>
2013-07-19 18:05:46 -0700

    Add singleton class to repo

---
+++ Singleton.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ 'taken from Advanced PHP Programming pg 57'
+**/
+class Singleton {
+    private static $props = array();
+
+    public function __construct() {}
+    public function __get($name)
+    {
+        if(array_key_exists($name, self::$props) {
+            return self::$props[$name];
+        }
+    }
+    public function __set($name, $value)
+    {
+        self::$props[$name] = $value;
+    }
+}
+
+?>
